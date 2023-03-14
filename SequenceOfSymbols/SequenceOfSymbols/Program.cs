18216c6960cb38c70682c9e5d5795da1d0715e17
Hojiakbar Mahmudov <hojimahmudov1102@gmail.com>
2023-03-14 13:18:56 +0500

    First commit

---
+++ SequenceOfSymbols/SequenceOfSymbols/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class MaxConsecutive
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: MaxConsecutive <sequence>");
+            Environment.Exit(1);
+        }
+
+        string sequence = args[0];
+        int maxCount = 1;
+        int currentCount = 1;
+
+        for (int i = 1; i < sequence.Length; i++)
+        {
+            if (sequence[i] != sequence[i - 1])
+            {
+                currentCount++;
+            }
+            else
+            {
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                }
+                currentCount = 1;
+            }
+        }
+
+        if (currentCount > maxCount)
+        {
+            maxCount = currentCount;
+        }
+
+        Console.WriteLine("Maximum number of unequal consecutive characters: " + maxCount);
+    }
+}
